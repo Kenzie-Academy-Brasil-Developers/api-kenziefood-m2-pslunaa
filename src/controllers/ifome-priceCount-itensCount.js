02e9f4ddc8d646f7efff760ff820c0c79287bd69
Luan_Dev <luanpbs98@gmail.com>
2022-01-26 09:45:43 -0300

    limpeza de codigo para merge

--- src/controllers/ifome-priceCount-itensCount.js
+++ src/controllers/ifome-priceCount-itensCount.js
@@ -1,78 +1,47 @@
 const carrinho = document.querySelector(".carrinho")
-const arrTest = [//array de teste
-    {
-        id              :   1,
-        nome            :   'pizza',
-        preco           :   20.00,
-    },
-    {
-        id              :   1,
-        nome            :   'hambuguer',
-        preco           :   30.00,
-    },
-    {
-        id              :   1,
-        nome            :   'macarrão',
-        preco           :   10.00,
-    },
-    {
-        id              :   1,
-        nome            :   'salgado',
-        preco           :   5.50,
-    },
-    {
-        id              :   1,
-        nome            :   'salgado',
-        preco           :   5.50,
-    },
-]
 
-class Count{
-    static cartFooter(arr){
-        if(arr.length !== 0){
-            let count = document.createElement('div');
-            count.id    = 'count';
-            count.classList.add('count');
+function cartFooter(arr){
+    if(arr.length !== 0){
+        let count = document.createElement('div');
+        count.id    = 'count';
+        count.classList.add('count');
 
-            let quantidade = document.createElement('div');
-            quantidade.id    = 'quantidade';
-            quantidade.classList.add('quantidade');
+        let quantidade = document.createElement('div');
+        quantidade.id    = 'quantidade';
+        quantidade.classList.add('quantidade');
              
-            let preco = document.createElement('div');
-            preco.id    = 'preco';
-            preco.classList.add('preco');
+        let preco = document.createElement('div');
+        preco.id    = 'preco';
+        preco.classList.add('preco');
 
-            let quantidadeTXT = document.createElement('h4');
-            quantidadeTXT.innerText = 'Quantidade'
+        let quantidadeTXT = document.createElement('h4');
+        quantidadeTXT.innerText = 'Quantidade'
 
-            let precoTXT = document.createElement('h4');
-            precoTXT.innerText = 'Total'
+        let precoTXT = document.createElement('h4');
+        precoTXT.innerText = 'Total'
 
-            let contador = document.createElement('h4');
-            contador.id  = 'itensCount'
-            contador.classList.add('itensCount')
-            contador.innerHTML = arr.length;
+        let contador = document.createElement('h4');
+        contador.id  = 'itensCount'
+        contador.classList.add('itensCount')
+        contador.innerHTML = arr.length;
 
-            let precoTotal = document.createElement('h4');
-            precoTotal.id  = 'precoCount'
-            precoTotal.classList.add('precoCount')
-            let price = 0;
-            console.log('hwllo')
-                for(let i=0; i < arr.length;i++){
-                    price += arr[i].preco
-                }
-            precoTotal.innerHTML = `R$ ${price.toFixed(2).toString().replace(".", ",")}`;
+        let precoTotal = document.createElement('h4');
+        precoTotal.id  = 'precoCount'
+        precoTotal.classList.add('precoCount')
+        let price = 0;
+            for(let i=0; i < arr.length;i++){
+                price += arr[i].preco
+            }
+        precoTotal.innerHTML = `R$ ${price.toFixed(2).toString().replace(".", ",")}`;
 
-            quantidade.appendChild(quantidadeTXT);
-            quantidade.appendChild(contador)
-            preco.appendChild(precoTXT);
-            preco.appendChild(precoTotal);
-            count.appendChild(quantidade);
-            count.appendChild(preco);
-            carrinho.appendChild(count);
-        }
+        quantidade.appendChild(quantidadeTXT);
+        quantidade.appendChild(contador)
+        preco.appendChild(precoTXT);
+        preco.appendChild(precoTotal);
+        count.appendChild(quantidade);
+        count.appendChild(preco);
+        carrinho.appendChild(count);
     }
 }
-Count.cartFooter(arrTest);
 
 
