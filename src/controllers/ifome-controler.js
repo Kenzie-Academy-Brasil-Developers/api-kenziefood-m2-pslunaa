f369a5ce3b04882c3f45b9175f65ab93d01bf4a5
Paulo Sousa <psluna.sousa@gmail.com>
2022-01-27 08:42:26 -0300

    Organizando link de rota privada

--- src/controllers/ifome-controler.js
+++ src/controllers/ifome-controler.js
@@ -1,24 +1,21 @@
 class KenzieFood {
 
-    static urlApi = "https://kenzie-food-api.herokuapp.com/product"
+    static urlApi = "https://kenzie-food-api.herokuapp.com/"
 
     static async getPublic() {
-        let response = await fetch(this.urlApi)
+        let response = await fetch(`${this.urlApi}product`)
         let data = await response.json()
         return data
     }
 
     static async getPrivate() {
-        let response = await fetch(this.urlApi, { headers :{Authorization: "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNjQzMDQ0MzQwLCJleHAiOjE2NDM5MDgzNDAsInN1YiI6IltvYmplY3QgVW5kZWZpbmVkXSJ9.9Sy4pQbIFQz92SjiMy2MY4IxTgoWC943u8fKdLXcBkE"}} )
+        let response = await fetch(`${this.urlApi}my/product`, { headers :{Authorization: "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiaWF0IjoxNjQzMDQ0MzQwLCJleHAiOjE2NDM5MDgzNDAsInN1YiI6IltvYmplY3QgVW5kZWZpbmVkXSJ9.9Sy4pQbIFQz92SjiMy2MY4IxTgoWC943u8fKdLXcBkE"}} )
         let data = await response.json()
         return data
     }
 
 }
 
-// KenzieFood.get().then(data=>{
-//     console.log(data)
-// })
 
 export{KenzieFood};
 
