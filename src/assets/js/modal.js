0d9877dec194aff49948df20badfc4809cbc95ec
Paulo Sousa <psluna.sousa@gmail.com>
2022-01-27 20:41:07 -0300

    Aplicando voltar para pagina do kenzie food

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -5,6 +5,38 @@ const modalPatch = document.querySelector(".modal-patch");
 const deleteBtn = document.querySelector(".delete");
 const modalDelete = document.querySelector(".modal-delete");
 
+const queroFecharPost = document.getElementById("queroFecharPost");
+const queroFecharPatch = document.getElementById("queroFecharPatch");
+const queroFecharDelete = document.getElementById("queroFecharDelete");
+
+//inputs Posts
+const nomePost = document.getElementById("nomePost");
+const descricaoPost = document.getElementById("descricaoPost");
+const precoPost = document.getElementById("precoPost");
+const categoriaPost = document.getElementById("categoriaPost");
+
+//inputs Patch
+const produtoPatch = document.getElementById("produto-patch"); 
+const produtoIdPatch = document.getElementById("produto-id-patch"); 
+const descricaoPatch = document.getElementById("descricao-patch"); 
+const precoPatch = document.getElementById("preco-patch"); 
+const imagemPatch = document.getElementById("imagem-patch"); 
+
+//input Delete
+const IDDelete = document.getElementById("IDDelete");
+
+queroFecharPost.addEventListener("click", () => {
+    modalPost.classList.remove('mod-post-active');
+})
+
+queroFecharPatch.addEventListener("click", () => {
+    modalPatch.classList.remove('mod-patch-active');    
+})
+
+queroFecharDelete.addEventListener("click", () => {
+    modalDelete.classList.remove('mod-delete-active');    
+})
+
 postBtn.addEventListener("click", function(){
     modalPost.classList.add('mod-post-active');
 })
@@ -14,3 +46,20 @@ patchBtn.addEventListener("click", function(){
 deleteBtn.addEventListener("click", function(){
     modalDelete.classList.add('mod-delete-active');
 })
+
+const corpo = document.querySelector("body");
+
+corpo.addEventListener("submit", pegarDadosInput);
+
+function pegarDadosInput(event){
+    event.preventDefault()
+    let inputs = event.target
+    let dataForm = {}
+
+    for(let i = 0; i < inputs.length-1; i++){
+        const {name, value} = inputs[i];
+        dataForm[name] = value;
+    }
+
+    console.log(dataForm)
+}
